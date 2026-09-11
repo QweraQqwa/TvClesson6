3aa79feb2bc21cc29bda005bb3c414fc265a3b0e
Nguyen Quoc Viet <xoi5441@gmail.com>
2026-09-12 05:08:08 +0700

    TvClesson6

---
+++ TvClesson6/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace TvClesson6.Models
+{
+    public class Category
+    {
+        public int categoryId { get; set; }
+        public string categoryName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ TvClesson6/ViewComponent/CategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TvClesson6.Models;
+
+namespace TvClesson6.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int? n)
+        {
+            var categories = new List<Category>
+            {
+                new Category { categoryId = 1, categoryName = "Electronics", IsActive = true },
+                new Category { categoryId = 2, categoryName = "Books", IsActive = true },
+                new Category { categoryId = 3, categoryName = "Clothing", IsActive = false },
+                new Category { categoryId = 4, categoryName = "Home & Kitchen", IsActive = true }
+            };
+            n = n ?? 0;
+            var search =categories.Where(c => c.categoryId>n).ToList();
+            return View(search);
+        }
+    }
+}
