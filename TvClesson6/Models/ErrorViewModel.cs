1e003862d32af6ae3314458846cf7953cd100546
Nguyen Quoc Viet <xoi5441@gmail.com>
2026-09-12 04:02:29 +0700

    Add project files.

---
+++ TvClesson6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TvClesson6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
